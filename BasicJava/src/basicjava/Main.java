b633e29b10034bf477d4c1b366f5b9676b885683
argojuni <juniargoprasaja05@gmail.com>
2023-10-11 18:52:50 +0700

    #4 tipe data dan variabel

--- BasicJava/src/basicjava/Main.java
+++ BasicJava/src/basicjava/Main.java
@@ -3,14 +3,19 @@ package basicjava;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Halo Semua");
-        System.out.println("Halo Semua");
-        
-        System.out.print("Halo Semua");
-        System.out.print("Halo Semua");
-        
-        System.out.println(6.5);
-        System.out.println(6*5);
+            System.out.println("6 + 5");
+            System.out.println(6+5);
+            
+//            integer
+            int umur = 22;
+            System.out.println(umur);
+            
+//            float
+            float angka = 5.5f;
+            System.out.println(angka);
+            
+//            penjumlahan
+            System.out.println(umur + angka);
     }
     
 }
