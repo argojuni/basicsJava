58cd83384422cb4941cdb66dbf21b77a84426635
argojuni <juniargoprasaja05@gmail.com>
2023-10-12 05:51:29 +0700

    #7 Input Data

--- BasicJava/src/basicjava/Main.java
+++ BasicJava/src/basicjava/Main.java
@@ -1,17 +1,28 @@
 package basicjava;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-            String nama = "Alexander\b";
-            int umur = 17;
-            double beratbadan = 71.5;
+            int angka1 ;
+            double angka2;
+            String nama;
+            Scanner input = new Scanner(System.in);
             
-            System.out.println("Nama \t\t= "+ nama);
-            System.out.println("Umur \t\t= "+umur+" tahun");
-            System.out.println("Berat Badan \t= "+beratbadan +" kg");
+            System.out.print("Masukan nama : ");
+            nama = input.nextLine();
             
-            System.out.println("Nama \t\t= " + nama + " \nUsia \t\t= "+ umur+" tahun");
+            System.out.print("Masukan angka1 : ");
+            angka1 = input.nextInt();
+            
+            System.out.print("Masukan angka2 : ");
+            angka2 = input.nextDouble();
+            
+            
+
+            System.out.println("angkat = " + angka1);
+            System.out.println("angkat = " + angka2);
+            System.out.println("nama = "+nama);
 
     }
     
