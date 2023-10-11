931fc824c83419be4b4175f37d117d67c142ce30
argojuni <juniargoprasaja05@gmail.com>
2023-10-11 18:25:57 +0700

    hello world

--- BasicJava/src/basicjava/Main.java
+++ BasicJava/src/basicjava/Main.java
@@ -8,13 +8,13 @@ package basicjava;
  *
  * @author JUNIARGO
  */
-public class BasicJava {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World");
     }
     
 }
