5df05434a000a6fd5b80b2eddd069cf6b5c59860
argojuni <juniargoprasaja05@gmail.com>
2023-10-12 09:18:49 +0700

    #11 menghitung luas persegi

--- BasicJava/src/basicjava/Main.java
+++ BasicJava/src/basicjava/Main.java
@@ -4,50 +4,24 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-            int a = 44;
-            int b = 22;
-            float c = 5.5f;
-            
-            int tambahB = a+b;
-            int kurangB = a-b;
-            int bagiB = a/b;
-            float kaliB = b*c;
-            float tambahD = c+a;
-            
-            System.out.println(tambahB);
-            System.out.println(kurangB);
-            System.out.println(tambahD);
-            System.out.println((double)bagiB);
-            System.out.println(kaliB);
-
-            
-            System.out.println("=============================");
             Scanner input = new Scanner(System.in);
             
-            int angka1, angka2,hasil;
+            int s, L,K;
             
-            System.out.print("Masukan angka1 = ");
-            angka1 = input.nextInt();
+            System.out.print("Masukan s : ");
+            s = input.nextInt();
+            // Luas Persegi
+            L = s*s;
+            // Keliling Persegi
+            K = 4*s;
             
-            System.out.print("Masukan angka2 = ");
-            angka2 = input.nextInt();
             
-            hasil = angka1 + angka2;
-            System.out.print(angka1+"+"+angka2+" = ");
-            System.out.println(hasil);
             
-            hasil = angka2- angka1;
-            System.out.println(angka1+"-"+angka2+" = "+hasil);
+            System.out.print("Luas Persegi = ");
+            System.out.println(L);
             
-            hasil = angka1*angka2;
-            System.out.println(angka1+"*"+angka2+" = "+hasil);
-            
-            hasil = angka2/angka1;
-            System.out.println(angka2+" : "+angka1+" = " + (float)hasil);
-            
-            System.out.println("Sisa Bagi / Modulus");
-            hasil = angka2 % angka1;
-            System.out.println(hasil);
+            System.out.print("Keliling Persegi = ");
+            System.out.println(K);
     }
     
 }
