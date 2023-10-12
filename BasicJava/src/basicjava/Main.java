84e35567abb86b086a81ad1029d02b2ff253416b
argojuni <juniargoprasaja05@gmail.com>
2023-10-12 08:30:31 +0700

    #10 operator aritmatika

--- BasicJava/src/basicjava/Main.java
+++ BasicJava/src/basicjava/Main.java
@@ -4,25 +4,50 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+            int a = 44;
+            int b = 22;
+            float c = 5.5f;
+            
+            int tambahB = a+b;
+            int kurangB = a-b;
+            int bagiB = a/b;
+            float kaliB = b*c;
+            float tambahD = c+a;
+            
+            System.out.println(tambahB);
+            System.out.println(kurangB);
+            System.out.println(tambahD);
+            System.out.println((double)bagiB);
+            System.out.println(kaliB);
+
+            
+            System.out.println("=============================");
             Scanner input = new Scanner(System.in);
             
-            String nama, alamat;
+            int angka1, angka2,hasil;
             
-            int umur;
+            System.out.print("Masukan angka1 = ");
+            angka1 = input.nextInt();
             
-            System.out.print("Masukan Nama\t: ");
-            nama = input.nextLine();
-            System.out.print("Masukan Alamat\t: ");
-            alamat = input.nextLine();
+            System.out.print("Masukan angka2 = ");
+            angka2 = input.nextInt();
             
-            System.out.print("Masukan Umur\t: ");
-            umur = input.nextInt();
+            hasil = angka1 + angka2;
+            System.out.print(angka1+"+"+angka2+" = ");
+            System.out.println(hasil);
             
-            System.out.println("===============================");
+            hasil = angka2- angka1;
+            System.out.println(angka1+"-"+angka2+" = "+hasil);
             
-            System.out.println(nama+" Lahir di "+alamat);
+            hasil = angka1*angka2;
+            System.out.println(angka1+"*"+angka2+" = "+hasil);
             
-            System.out.println("Berumur "+umur+" th.");
+            hasil = angka2/angka1;
+            System.out.println(angka2+" : "+angka1+" = " + (float)hasil);
+            
+            System.out.println("Sisa Bagi / Modulus");
+            hasil = angka2 % angka1;
+            System.out.println(hasil);
     }
     
 }
